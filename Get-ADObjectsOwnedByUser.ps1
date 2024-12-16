1a318a279171019d2617fc25128e73084d7729d1
Tim McGuffin <17074002+NotMedic@users.noreply.github.com>
2024-12-16 15:59:23 -0600

    Update Get-ADObjectsOwnedByUser.ps1
    Filter by AD Object type.

--- Get-ADObjectsOwnedByUser.ps1
+++ Get-ADObjectsOwnedByUser.ps1
@@ -1,4 +1,4 @@
- function Get-ADObjectsOwnedByUser {
+  function Get-ADObjectsOwnedByUser {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory=$true)]
@@ -9,13 +9,11 @@
 
     $user = Get-ADUser -Identity $Username
 
-    ForEach ($obj in Get-ADObject -Filter * -Properties Owner) {
+    ForEach ($obj in Get-ADObject -Filter { (Name -eq $ObjectName -or SamAccountName -eq $ObjectName ) -and (objectClass -eq 'user' -or objectClass -eq 'computer' -or objectClass -eq 'group') } -Properties Owner) {
         $acl = Get-ACL -Path ("AD:\" + $obj.DistinguishedName) -ErrorAction SilentlyContinue
 
         if ($user.sid.value -eq ($acl.Sddl -replace 'o:(.+?)G:.+','$1')) {
             write-host $obj.Name is owned by $acl.Owner
         }
     }
-}
-
- 
+} 
